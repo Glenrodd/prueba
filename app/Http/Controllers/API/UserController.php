5b12f9c5abb0fdc2b07f7467d55e13352718ab54
Glenrodd <roddwy@gmail.com>
2020-01-21 03:03:19 -0400

    Cambios

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -23,7 +23,7 @@ class UserController extends Controller
             return response()->json(['success' => $success], $this-> successStatus); 
         } 
         else{ 
-            return response()->json(['error'=>'Unauthorised'], 401); 
+            return response()->json(['error'=>'No Autorizado'], 401); 
         } 
     }
 	/** 
